c96253bef68dcf59feac89dfd87a38fa859add06
ahmed elkomy <ahmedelkomy@gmail.com>
2024-01-17 14:39:15 +0200

    AndroidAssignment#1

--- app/src/main/java/com/route/assignment_1/LoginActivity.java
+++ app/src/main/java/com/route/assignment_1/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.route.assignment_1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/route/assignment_1/PostItemActivity.java
+++ app/src/main/java/com/route/assignment_1/PostItemActivity.java
@@ -2,13 +2,25 @@ package com.route.assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PostItemActivity extends AppCompatActivity {
 
+    Button btn_go;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_item);
+        btn_go=findViewById(R.id.btn_id);
+        Intent intent = new Intent(this,LoginActivity.class);
+        btn_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
